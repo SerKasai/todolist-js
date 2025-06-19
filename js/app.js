312bfb7f9f30f0ec54411ad80803443967276c1d
SerKasai <serjtheignis92@gmail.com>
2025-06-19 15:57:24 +0200

    finish

---
+++ js/app.js
@@ -0,0 +1,24 @@
+console.log('To Do List')
+
+const app = new Vue({
+  el: '#app',
+  data: {
+    newTodo: '',
+    todoList: [
+      'Fare la spesa',
+      'Fare i compiti',
+      'Buttare la spazzatura'
+    ]
+  },
+  methods: {
+    addTodo: function () {
+      if (this.newTodo !== '') {
+        this.todoList.push(this.newTodo);
+        this.newTodo = '';
+      }
+    },
+    deleteTodo: function (i) {
+      this.todoList.splice(i, 1);
+    }
+  }
+})
